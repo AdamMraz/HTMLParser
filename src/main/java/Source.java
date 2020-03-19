dd7a83a5c2a3ce37705abdabe7753b39ae39b16c
AdamMraz <59054958+AdamMraz@users.noreply.github.com>
2020-03-19 12:57:44 +0700

    Pattern check added.

--- src/main/java/Source.java
+++ src/main/java/Source.java
@@ -8,26 +8,31 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by AdamMraz on 3/18/2020.
  */
 public class Source {
     public static void main (String[] args) throws IOException {
+        String pattern = ".*((\\.png)||(\\.jpg))";
         Document doc = (Document) Jsoup.connect("https://lenta.ru/").get();
         Elements elements = doc.select("img");
         elements.forEach(element -> {
             if (element.attr("src").contains("http")
-                    && (element.attr("src").contains("jpg")
-                    || element.attr("src").contains("png"))) {
+                    && (element.attr("src").contains(".jpg")
+                    || element.attr("src").contains(".png"))) {
                 String[] name = element.attr("src").split("/");
-                try {
-                    URL url = new URL(element.attr("src"));
-                    InputStream inputStream = url.openStream();
-                    Files.copy(inputStream, Paths.get("img/" + name[name.length - 1]),
-                            StandardCopyOption.REPLACE_EXISTING);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if(Pattern.matches(pattern, name[name.length - 1])) {
+                    try {
+                        URL url = new URL(element.attr("src"));
+                        InputStream inputStream = url.openStream();
+                        Files.copy(inputStream, Paths.get("img/" + name[name.length - 1]),
+                                StandardCopyOption.REPLACE_EXISTING);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
